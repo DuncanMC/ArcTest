ccef28d8e92f8f0b968b1ee23631dffe4e4d82a9
Duncan Champney <dchampney@wareto.com>
2015-01-20 08:46:42 -0500

    Initial Commit

---
+++ ArcTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ArcTest
+//
+//  Created by Duncan Champney on 1/20/15.
+//  Copyright (c) 2015 WareTo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ArcTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ArcTest
+//
+//  Created by Duncan Champney on 1/20/15.
+//  Copyright (c) 2015 WareTo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
